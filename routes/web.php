82d91f45cf2fb17919d42bd0c4e5a46131ff9c70
jugal-chanda <jugalchanda7@gmail.com>
2020-12-26 12:25:32 +0600

    UPload Code

--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,7 @@ Route::resource('contentDeveloperProfile', 'ContentDeveloperProfileController')-
 // Route::get('student/subjects/','SubjectController@studentSubjectsAll')->middleware('roleauth')->name('student.subjects.all');
 
 Route::get('/question/{learn}','QuestionController@create')->name('question.create')->middleware('roleauth');
-Route::get('/question/{learn}/edit','QuestionController@edit')->name('question.edit');
+Route::get('/question/{learn}/edit','QuestionController@edit')->name('question.edit')->middleware('roleauth');
 
 
 // Route::resource('bid', 'BidContentController')->middleware('roleauth');
