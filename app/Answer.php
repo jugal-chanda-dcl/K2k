b1e522080aea2ddc344d113dd886d1363a0711d7
jugal-chanda <jugalchanda7@gmail.com>
2021-02-04 16:24:42 +0600

    Fix bug on answer model

--- app/Answer.php
+++ app/Answer.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-  $fillable = ['user_id','question_id','answer','answered','score','total','checked'];
+  protected $fillable = ['user_id','question_id','answer','answered','score','total','checked'];
   public function user()
   {
     return $this->belongsTo('App\User');
