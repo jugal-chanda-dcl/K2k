c40c399143eea201cb8b4a8af99418761a647b96
jugal-chanda-dcl <jugalchanda.dcl@gmail.com>
2021-03-17 15:38:31 +0600

    Update topic view

--- resources/views/practice/topic_questions.blade.php
+++ resources/views/practice/topic_questions.blade.php
@@ -24,12 +24,24 @@
     <ol>
       @foreach($subject->topics as $topic)
       @if($topic->learn->question)
+      @php
+        $attempts = $topic->learn->question->answers()->where('user_id',Auth::user()->id)->count();
+        $limit = $topic->learn->question->practice_limit;
+      @endphp
       <li>
         <a href="{{ route('answer.submit_view',['question'=>$topic->learn->question]) }}">Practice {{ $topic->name }}</a>
+        <span>(<span class="text-danger text-bold">{{ $attempts }}</span> Attempts <span class="text-success">{{ $limit-$attempts }}</span> Remaining)</span>
         @if($topic->learn->question->answers()->where('user_id',Auth::user()->id)->count())
           <ul>
             @foreach($topic->learn->question->answers()->where('user_id',Auth::user()->id)->get() as $answer)
-              <li><span>Score: {{  $answer->score  }} out of {{ $answer->total }}</span></li>
+              <li>
+                <span>Score: {{  $answer->score  }} out of {{ $answer->total }}</span>
+                @if($answer->checked)
+                <span class="text-success">(Script Checked)</span>
+                @else
+                <span class="text-danger">(Script Not Checked By Teacher)</span>
+                @endif
+              </li>
             @endforeach
           </ul>
         @endif
